c09105bd8f47388ba03a345b8ddcd87d14f1b97d
DK <dmitry.koleev@game-garden.com>
2020-07-27 07:23:02 +0300

    Init

---
+++ AlgorihtmsAndStructures/src/BinaryTree/Node.java
@@ -0,0 +1,15 @@
+package BinaryTree;
+
+public class Node {
+    private INodeData _data;
+    private Node Left;
+    private Node Right;
+
+    public void DisplayNode(){
+
+    }
+
+    public INodeData GetData(){
+        return _data;
+    }
+}
